5f2cdfcf6ec3bbfd05621f3a3696ba6746f59e8e
MonikaZamyslowska <zamyslowska.monika@gmail.com>
2018-06-22 21:26:58 +0200

    correct bugs #1

--- kodilla-stream/src/main/java/com/kodilla/stream/world/Continent.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/Continent.java
@@ -1,26 +1,25 @@
 package com.kodilla.stream.world;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public final class Continent {
+    private final String nameContinent;
     private final Set<Country> countries = new HashSet<>();
-    private final BigDecimal populations;
 
-    public BigDecimal getPopulations() {
-        return BigDecimal populations = countries.stream()
-                .map(country -> country.getPeopleQuantity())
-                .forEach();
+    public Continent(final String nameContinent) {
+        this.nameContinent = nameContinent;
     }
 
-    public  Set<String> getCountries() {
-        return countries.stream()
-                .map(country -> country.getNameCountry())
-                .collect(Collectors.toSet());
+    public void addCountries(Country country) {
+        countries.add(country);
     }
 
+    public String getNameContinent() {
+        return nameContinent;
+    }
+
+    public Set<Country> getCountries() {
+        return countries;
+    }
 }
--- kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/world/World.java
@@ -1,26 +1,28 @@
 package com.kodilla.stream.world;
 
+
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
-public class World {
+public final class World {
     private final Set<Continent> continents = new HashSet<>();
-    private final BigDecimal peopleQuantity;
 
-    public Set<String> getContinents() {
-       return continents.stream()
+    public  void addContinent(Continent continent) {
+        continents.add(continent);
+    }
+
+    public BigDecimal getPeopleQuantity() {
+        BigDecimal peopleQuantity = continents.stream()
                 .flatMap(continent -> continent.getCountries().stream())
-                .collect(Collectors.toSet());
+                .map(country -> country.getPeopleQuantity())
+                .reduce(BigDecimal.ZERO, (sum, current) -> sum.add(current));
+        return peopleQuantity;
     }
 
-    public BigDecimal getPeopleQuantity(Country populations) {
-        return BigDecimal peopleQuantity = continents.stream()
-                .flatMap(continent -> continent.getCountries())
-                .reduce(BigDecimal.ZERO, )
+    public Set<Continent> getContinents() {
+        return continents;
     }
+
 
 }
