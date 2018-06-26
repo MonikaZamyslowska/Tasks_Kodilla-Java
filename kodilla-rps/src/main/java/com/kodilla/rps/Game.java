60b7fd298f6db033967d287a48c9c9b8947a52f2
MonikaZamyslowska <zamyslowska.monika@gmail.com>
2018-06-26 14:44:18 +0200

    draft scetches #3

--- kodilla-rps/src/main/java/com/kodilla/rps/Game.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/Game.java
@@ -1,8 +1,10 @@
 package com.kodilla.rps;
 
+import java.util.Scanner;
+
 public class Game {
-    Player player;
-    Computer computer;
+    Player player = new Player();
+    Computer computer = new Computer();
     private int computerScore;
     private int playerScore;
     private int numberOfGames;
@@ -12,14 +14,61 @@ public class Game {
     }
 
     public Game() {
-        Player player = new Player();
-        Computer computer = new Computer();
         playerScore = 0;
         computerScore = 0;
         numberOfGames = 0;
     }
 
-    public int compere(Player playerChoice, Computer computerChoice) {
+    public boolean endGame() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Are you sure want to end the game? /n YES = y /n NO = n");
+        Character end = scanner.next().charAt(0);
+        return end == 'y';
+    }
+
+    public boolean resetGame() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Are you sure want to reset Game, and play once again? /n YES = y /n NO = /n");
+        Character reset = scanner.next().charAt(0);
+        return reset == 'y';
+    }
+
+    public boolean playAgain() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Do you want play again? /n Yes = y /n NO = /n");
+        Character again = scanner.next().charAt(0);
+        return again == 'y';
+    }
+
+    public void display(String name, Choice playerChoice) {
+        name = player.getName();
+        switch (playerChoice) {
+            case ROCK:
+                System.out.println(name + " selected ROCK ");
+            case PAPER:
+                System.out.println(name + " selected Paper ");
+            case SCISSOR:
+                System.out.println(name + " selected Scissor");
+            case RESET:
+                resetGame();
+            case END:
+                endGame();
+            }
+        }
+
+
+    public int compere(Choice choicePlayer, Choice choiceComputer) {
+        choicePlayer = player.playerChoice();
+        choiceComputer = computer.computerChoice();
+        if (choicePlayer.equals(choiceComputer)) return 0;
+        switch (choicePlayer) {
+            case ROCK:
+                return (choiceComputer == Choice.SCISSOR ? 1 : -1);
+            case PAPER:
+                return (choiceComputer == Choice.ROCK ? 1 : -1);
+            case SCISSOR:
+                return (choiceComputer == Choice.PAPER ? 1 : -1);
+        }
         return 0;
     }
 
--- kodilla-rps/src/main/java/com/kodilla/rps/Player.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/Player.java
@@ -1,6 +1,5 @@
 package com.kodilla.rps;
 
-import java.awt.*;
 import java.util.Scanner;
 
 public class Player {
@@ -23,36 +22,18 @@ public class Player {
 
     public Game.Choice playerChoice() {
         Character playerChoice;
-        System.out.println("Select: /n ROCK = 1 /n PAPER = 2 /n SCISSOR = 3 /n END GAME = y /n RESET GAME = x");
+        System.out.println("Select: /n ROCK = 1 /n PAPER = 2 /n SCISSOR = 3 /n END GAME = x /n RESET GAME = n");
         playerChoice = scanner.next().charAt(0);
         switch (playerChoice) {
             case '1': return Game.Choice.ROCK;
             case '2': return Game.Choice.PAPER;
             case '3': return Game.Choice.SCISSOR;
             case 'x': return Game.Choice.END;
-            case 'y': return Game.Choice.RESET;
+            case 'n': return Game.Choice.RESET;
         }
         return playerChoice();
     }
 
-    public boolean endGame() {
-        //scanner = new Scanner(System.in);
-        System.out.println("Are you sure want to end the game? /n YES = y /n NO = n");
-        Character end = scanner.next().charAt(0);
-        return end == 'y';
-    }
-
-    public boolean resetGame() {
-        System.out.println("Are you sure want to reset Game, and play once again? /n YES = y /n NO = /n");
-        Character reset = scanner.next().charAt(0);
-        return reset == 'y';
-    }
-
-    public boolean playAgain() {
-        System.out.println("Do you want play again? /n Yes = y /n NO = /n");
-        Character again = scanner.next().charAt(0);
-        return again == 'y';
-    }
 
     public String getName() {
         return name;
