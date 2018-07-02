7174b4a874bd428be6876f868499792099c9fc84
MonikaZamyslowska <zamyslowska.monika@gmail.com>
2018-07-02 10:48:23 +0200

    solving tasks from the module 8

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -0,0 +1,25 @@
+package com.kodilla.exception.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class FileReader {
+    public void readFile() {
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("file/name.txt").getFile());
+
+        try (Stream<String> fileLine = Files.lines(Paths.get(file.getPath()))){
+            fileLine.forEach(System.out::println);
+        } catch (IOException e) {
+            System.out.println("Something went wrong!!! " + e);
+        } finally {
+            System.out.println("I`m gonna be here... ALWAYS!!!");
+        }
+
+    }
+}
