dfe45f83ccd700a6ead8479bc9e15f402e4e7bb7
MonikaZamyslowska <zamyslowska.monika@gmail.com>
2018-07-11 20:12:04 +0200

    Initial implementation class OptionGame

--- kodilla-game-rps/src/main/java/com/kodilla/rps/game/OptionGame.java
+++ kodilla-game-rps/src/main/java/com/kodilla/rps/game/OptionGame.java
@@ -1,16 +1,29 @@
 package com.kodilla.rps.game;
 
+import java.util.Scanner;
+
 public class OptionGame {
+    private final char YES = 'y';
+    private final char NO = 'n';
 
     public boolean endGame(){
-        return false;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Are you sure want to end the game? \n YES = y \n NO = n");
+        Character theEnd = scanner.next().charAt(0);
+        return theEnd == YES;
     }
 
     public boolean resetGame() {
-        return false;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Are you sure want to reset Game, and play once again? \n YES = y \n NO = n");
+        Character reset = scanner.next().charAt(0);
+        return reset == YES;
     }
 
     public boolean playAgain() {
-        return false;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Do you want play again? \n Yes = y \n NO = n");
+        Character again = scanner.next().charAt(0);
+        return again == YES;
     }
 }
