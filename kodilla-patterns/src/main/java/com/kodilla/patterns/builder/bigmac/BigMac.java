a802ea6b0469aed1bc87a002fb3969872fef3391
MonikaZamyslowska <zamyslowska.monika@gmail.com>
2018-07-24 10:13:12 +0200

    Correction of methods, create construktor in BigMacBuilder class

--- kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/BigMac.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/builder/bigmac/BigMac.java
@@ -1,36 +1,55 @@
 package com.kodilla.patterns.builder.bigmac;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BigMac {
     private final String bun;
     private final String sauce;
     private final List<String> ingredients;
 
-    final static String BUN_WITH_SESAME = "with sesame";
-    final static String BUN_NO_SESAME = "no sesame";
-    final static String SAUCE_STANDARD = "standard";
-    final static String SAUCE_THOUSAND_ISLAND = "thousand island";
-    final static String SAUCE_BARBECUE = "barbecue";
-    final static String INGREDIENT_SALAD = "salad";
-    final static String INGREDIENT_ONION = "onion";
-    final static String INGREDIENT_BECKON = "beckon";
-    final static String INGREDIENT_CUCUMBER = "cucumber";
-    final static String INGREDIENT_CHILI = "chili";
-    final static String INGREDIENT_MUSHROOMS = "mushrooms";
-    final static String INGREDIENT_SHRIMP = "shrimp";
-    final static String INGREDIENT_CHEESE = "cheese";
-    final static String EXCEPTION_COMMUNIQUE = "Please choose once again, illegal component.";
+    public final static String BUN_WITH_SESAME = "with sesame";
+    public final static String BUN_NO_SESAME = "no sesame";
+    public final static String SAUCE_STANDARD = "standard";
+    public final static String SAUCE_THOUSAND_ISLAND = "thousand island";
+    public final static String SAUCE_BARBECUE = "barbecue";
+    public final static String INGREDIENT_SALAD = "salad";
+    public final static String INGREDIENT_ONION = "onion";
+    public final static String INGREDIENT_BECKON = "beckon";
+    public final static String INGREDIENT_CUCUMBER = "cucumber";
+    public final static String INGREDIENT_CHILI = "chili";
+    public final static String INGREDIENT_MUSHROOMS = "mushrooms";
+    public final static String INGREDIENT_SHRIMP = "shrimp";
+    public final static String INGREDIENT_CHEESE = "cheese";
+    public final static String EXCEPTION_COMMUNIQUE = "Please choose once again, illegal component.";
 
 
     public static class BigMacBuilder {
         private String bun;
         private String sauce;
         private List<String> ingredients = new ArrayList<>();
+        private final Set<String> products = new HashSet<>();
+
+        public BigMacBuilder() {
+            products.add(BUN_WITH_SESAME);
+            products.add(BUN_NO_SESAME);
+            products.add(SAUCE_BARBECUE);
+            products.add(SAUCE_STANDARD);
+            products.add(SAUCE_THOUSAND_ISLAND);
+            products.add(INGREDIENT_BECKON);
+            products.add(INGREDIENT_CHEESE);
+            products.add(INGREDIENT_CHILI);
+            products.add(INGREDIENT_CUCUMBER);
+            products.add(INGREDIENT_MUSHROOMS);
+            products.add(INGREDIENT_ONION);
+            products.add(INGREDIENT_SALAD);
+            products.add(INGREDIENT_SHRIMP);
+        }
 
         public BigMacBuilder bun(String bun) {
-            if (bun.equals(BUN_NO_SESAME) || bun.equals(BUN_WITH_SESAME)) {
+            if (products.contains(bun)) {
                 this.bun = bun;
             } else {
                 throw new IllegalStateException(EXCEPTION_COMMUNIQUE);
@@ -39,7 +58,7 @@ public class BigMac {
         }
 
         public BigMacBuilder sauce(String sauce) {
-            if (sauce.equals(SAUCE_BARBECUE) || sauce.equals(SAUCE_STANDARD) || sauce.equals(SAUCE_THOUSAND_ISLAND)) {
+            if (products.contains(sauce)) {
                 this.sauce = sauce;
             } else {
                 throw new IllegalStateException(EXCEPTION_COMMUNIQUE);
@@ -48,10 +67,7 @@ public class BigMac {
         }
 
         public BigMacBuilder ingredient(String ingredient) {
-            if (ingredient.equals(INGREDIENT_BECKON) || ingredient.equals(INGREDIENT_CHEESE) ||
-                    ingredient.equals(INGREDIENT_CHILI) || ingredient.equals(INGREDIENT_CUCUMBER) ||
-                    ingredient.equals(INGREDIENT_MUSHROOMS) || ingredient.equals(INGREDIENT_ONION) ||
-                    ingredient.equals(INGREDIENT_SALAD) || ingredient.equals(INGREDIENT_SHRIMP)) {
+            if (products.contains(ingredient)) {
                 this.ingredients.add(ingredient);
             } else {
                 throw new IllegalStateException(EXCEPTION_COMMUNIQUE);
